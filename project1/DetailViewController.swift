7f725cc4f5a183c2323368720229259579a9b5ea
Akshay Mahajan <maha0134@algonquinlive.com>
2023-05-14 19:37:16 -0400

    add DetailView

---
+++ project1/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  project1
+//
+//  Created by AKSHAY MAHAJAN on 2023-05-14.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+	@IBOutlet var imageView: UIImageView!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
