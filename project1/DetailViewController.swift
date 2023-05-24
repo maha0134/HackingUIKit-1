36d38eb3a763eb5f7995d8a61bf5f8906d50138a
Akshay Mahajan <maha0134@algonquinlive.com>
2023-05-24 16:16:21 -0400

    add share bar button

--- project1/DetailViewController.swift
+++ project1/DetailViewController.swift
@@ -16,7 +16,9 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		navigationItem.largeTitleDisplayMode = .never
-
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+		
 		if let selectedImageIndex, let totalImages {
 			title = "Picture \(selectedImageIndex) of \(totalImages)"
 		}
@@ -34,14 +36,16 @@ class DetailViewController: UIViewController {
 		super.viewWillDisappear(animated)
 		navigationController?.hidesBarsOnTap = false
 	}
-    /*
-    // MARK: - Navigation
+	
+	@objc func shareTapped() {
+		guard let image = imageView.image?.jpegData(compressionQuality: 0.8), let imageName = selectedImage else {
+			print("No image found")
+			return
+		}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+		let vc = UIActivityViewController(activityItems: [image, imageName], applicationActivities: [])
+		vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+		present(vc, animated: true)
+	}
 
 }
